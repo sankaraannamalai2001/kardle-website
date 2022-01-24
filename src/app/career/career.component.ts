bbcb0023f642ceb769df89801520a4f5c3f08833
sankaraannamalai2001 <81792506+sankaraannamalai2001@users.noreply.github.com>
2022-01-24 09:14:23 +0530

    career page changes

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-career',
   templateUrl: './career.component.html',
-  styleUrls: ['./career.component.css']
+  styleUrls: ['./career.component.css'],
 })
 export class CareerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
